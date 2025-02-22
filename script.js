054e35b9ac0e7b17c5442fb47779e5452d579c85
javiers01 <hello@zinkvision.com>
2025-02-22 20:28:58 +0100

    Revert "script"
    This reverts commit d48157b251b7ff914d78a10ad13372f939cd390f.

--- script.js
+++
@@ -1,8 +0,0 @@
-// Select elements
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-// Toggle menu on click
-menuToggle.addEventListener("click", function () {
-    navLinks.classList.toggle("nav-active");
-});
